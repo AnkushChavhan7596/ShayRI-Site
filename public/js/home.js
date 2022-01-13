70e65e2fa70da5003702bcba3e1e18e0d828fa87
Ankush Chavhan <77318013+AnkushChavhan7596@users.noreply.github.com>
2022-01-13 10:31:54 -0800

    file updated

--- public/js/home.js
+++ public/js/home.js
@@ -3,3 +3,13 @@ const currentYear = document.querySelector(".current__year__footer");
 const date = new Date();
 
 currentYear.textContent = date.getFullYear();
+
+
+// ============= 
+const hambergur = document.querySelector(".hambergur");
+
+const sidebar_menu = document.querySelector(".menu__container");
+
+hambergur.addEventListener("click", ()=>{
+    sidebar_menu.classList.toggle("show_sidebar_menu");
+});
